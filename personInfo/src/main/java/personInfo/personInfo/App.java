8eb9ccceaa17e07499ce5de12358af7ea2d50bc1
Shyam Bharti <shyam.k.bharti@gmail.com>
2022-06-03 09:13:38 -0700

    Added new changes

---
+++ personInfo/src/main/java/personInfo/personInfo/App.java
@@ -0,0 +1,14 @@
+package personInfo.personInfo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World! 1 June" );
+    }
+}
