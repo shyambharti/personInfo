030ad5fbc9d02f0dce87fa639c7b99f2c9813b43
Shyam Bharti <shyam.k.bharti@gmail.com>
2022-06-03 09:16:46 -0700

    updated

--- personInfo/src/main/java/personInfo/personInfo/App.java
+++ personInfo/src/main/java/personInfo/personInfo/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println( "Hello World! 1 June" );
+        System.out.println( "Hello World! 3 June" );
     }
 }
